190b18b8cd3e8af9d8060515b68cb91e533b0bc3
atcamo <atiliocm@gmail.com>
2024-01-27 05:04:03 -0300

    color ok

---
+++ src/data/menuItemsData.ts
@@ -0,0 +1,22 @@
+import type { MenuItem } from "../types/types";
+
+const menuItems :MenuItem []= [
+  {
+    title: 'HOME',
+    url:'/',
+  },
+  {
+    title: 'BEE',
+    url:'/bee',
+  },
+  {
+    title: 'NOUNS',
+    url:'/nouns',
+  },
+  {
+    title: 'FAQ',
+    url:'/faq',
+  }
+]
+
+export default menuItems;
